caab409202981b70bc5fc8fb0a13910b9628e527
Jiyan Wang <jiyawang@2-A-2.42heilbronn.de>
2026-01-27 13:47:22 +0100

    fixed_exit

--- executor/built_in/mis_cd.c
+++ executor/built_in/mis_cd.c
@@ -6,7 +6,7 @@
 /*   By: jiyawang <jiyawang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 18:45:31 by jiyawang          #+#    #+#             */
-/*   Updated: 2026/01/27 13:02:38 by jiyawang         ###   ########.fr       */
+/*   Updated: 2026/01/27 13:22:57 by jiyawang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,49 +27,6 @@ static void	update_pwd(t_minishell *shell, char *key, char *value)
 	add_to_env(shell, entry);
 	free(entry);
 }
-
-// static char	*handle_cd_args(char *arg, t_minishell *shell)
-// {
-// 	char	*dir;
-// 	char	*home;
-// 	size_t	home_len;
-// 	size_t	arg_len;
-
-// 	if (!arg || (ft_strncmp(arg, "~", 2) == 0))
-// 	{
-// 		dir = get_env_value(shell->env, "HOME");
-// 		if (!dir)
-// 			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-// 		return (dir);
-// 	}
-// 	if (arg && arg[0] == '~' && (arg[1] == '/' || arg[1] == '\0'))
-// 	{
-// 		home = get_env_value(shell->env, "HOME");
-// 		if (!home)
-// 		{
-// 			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-// 			return (NULL);
-// 		}
-// 		home_len = ft_strlen(home);
-// 		arg_len = ft_strlen(arg);
-// 		char *expanded = malloc(home_len + arg_len);
-// 		if (!expanded)
-// 			return (NULL);
-// 		ft_strlcpy(expanded, home, home_len + arg_len);
-// 		ft_strlcat(expanded, arg + 1, home_len + arg_len);
-// 		return (expanded);
-// 	}
-// 	else if (ft_strncmp(arg, "-", 2) == 0)
-// 	{
-// 		dir = get_env_value(shell->env, "OLDPWD");
-// 		if (!dir)
-// 			ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
-// 		else
-// 			ft_putendl_fd(dir, STDOUT_FILENO);
-// 		return (dir);
-// 	}
-// 	return (arg);
-// }
 
 static void	update_pwds(t_minishell *shell, char *old_pwd)
 {
--- executor/built_in/mis_exit.c
+++ executor/built_in/mis_exit.c
@@ -6,7 +6,7 @@
 /*   By: jiyawang <jiyawang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 19:54:41 by jiyawang          #+#    #+#             */
-/*   Updated: 2026/01/27 12:40:09 by jiyawang         ###   ########.fr       */
+/*   Updated: 2026/01/27 13:42:12 by jiyawang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,23 @@ long long	ft_atoll(const char *str)
 	return ((long long)(result * sign));
 }
 
+static int	handle_non_numeric_exit(char *arg, t_minishell *shell)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	shell->exit_status = 2;
+	shell->should_exit = 1;
+	return (1);
+}
+
+static int	handle_too_many_args(t_minishell *shell)
+{
+	ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+	shell->exit_status = 1;
+	shell->should_exit = 1;
+	return (1);
+}
 void	mis_exit(t_command *cmd, t_minishell *shell)
 {
 	if (isatty(STDIN_FILENO))
@@ -62,22 +79,14 @@ void	mis_exit(t_command *cmd, t_minishell *shell)
 	if (cmd->args[1])
 	{
 		if (!is_numeric(cmd->args[1]))
-		{
-			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-			ft_putstr_fd(cmd->args[1], STDERR_FILENO);
-			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-			shell->exit_status = 2;
-			return ;
-		}
+			if (handle_non_numeric_exit(cmd->args[1], shell))
+				return ;
 		if (cmd->args[2])
-		{
-			ft_putstr_fd("minishell: exit: too many arguments\n",
-				STDERR_FILENO);
-			shell->exit_status = 1;
-			return ;
-		}
+			if (handle_too_many_args(shell))
+				return ;
 		shell->exit_status = (unsigned char)ft_atoll(cmd->args[1]);
+		shell->should_exit = 1;
 		return ;
 	}
-	return ;
+	shell->should_exit = 1;
 }
